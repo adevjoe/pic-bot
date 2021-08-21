5d3d67fbcaa694b82d20a693cafd5ba6639587a1
adevjoe <adevjoe@gmail.com>
2021-08-22 01:20:09 +0800

    Add welcome message

--- pkg/handle/start.go
+++ pkg/handle/start.go
@@ -1,9 +1,12 @@
 package handle
 
-import tb "gopkg.in/tucnak/telebot.v2"
+import (
+	tb "gopkg.in/tucnak/telebot.v2"
+	"os"
+)
 
 func Start(bot *tb.Bot, message *tb.Message) {
-	_, _ = bot.Send(message.Sender, "欢迎使用图片分享 Bot！您可以直接发送图片来投稿，也可以发送社交媒体的分享链接（目前支持 Twitter、Instagram、Weibo），如：https://twitter.com/alracoco/status/1421829535532687365", &tb.SendOptions{
+	_, _ = bot.Send(message.Sender, os.Getenv("WELCOME_MESSAGE"), &tb.SendOptions{
 		DisableWebPagePreview: true,
 	})
 }
